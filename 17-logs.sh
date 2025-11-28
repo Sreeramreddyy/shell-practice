4847699d7d327b79867e4a4d21765c56693f19f1
Sreeram Reddy <sreeramlearning25@gmail.com>
2025-11-29 00:13:08 +0530

    Variables

--- 17-logs.sh
+++ 17-logs.sh
@@ -12,7 +12,7 @@ LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOGS_FOLDER
 
-echo "Script started executed at: $(date)"
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
 
 if [ $USERID -ne 0 ]; then
     echo "ERROR:: Please run this script with root privelege"
@@ -21,10 +21,10 @@ fi
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then 
-        echo -e "ERROR:: Installing $2 ...$R FAILURE $N"
+        echo -e "ERROR:: Installing $2 ...$R FAILURE $N" | tee -a $LOG_FILE
         exit 1
     else 
-        echo -e "Installing $2 ... $G SUCCESS $N"    
+        echo -e "Installing $2 ... $G SUCCESS $N" | tee -a $LOG_FILE
     fi    
 }
 
@@ -33,7 +33,7 @@ if [ $? -ne 0 ]; then
     dnf install mysql -y &>>$LOG_FILE
     VALIDATE $? "MySQL"
 else
-    echo -e "Mysql already exit... $Y SKIPPING $N"
+    echo -e "Mysql already exit... $Y SKIPPING $N" | tee -a $LOG_FILE
 fi    
 
 dnf list installed nginx &>>$LOG_FILE
@@ -41,12 +41,12 @@ if [ $? -ne 0 ]; then
     dnf install nginx -y &>>$LOG_FILE
     VALIDATE $? "Nginx"
 else
-    echo -e "Nginx already exit... $Y SKIPPING $N"
+    echo -e "Nginx already exit... $Y SKIPPING $N" | tee -a $LOG_FILE
 fi 
 dnf list installed python3 &>>$LOG_FILE
 if [ $? -ne 0 ]; then
     dnf install python3 -y &>>$LOG_FILE
     VALIDATE $? "Python3"
 else
-    echo -e "Python3 already exit... $Y SKIPPING $N"
+    echo -e "Python3 already exit... $Y SKIPPING $N" | tee -a $LOG_FILE
 fi 
