0e3f14245d68be6f7813f43035f6d8e31d822133
Sreeram Reddy <sreeramlearning25@gmail.com>
2025-11-28 22:18:28 +0530

    Variables

---
+++ 13-installation.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USER -ne ]; then
+    echo "ERROR:: Please run this script with root privelege"
+fi
+
+dnf install mysql -y
+
+if [ $? -ne ]; then 
+    echo "ERROR:: Installing MySQL is failure"
+else 
+    echo "Installing MySQL is SUCCESS"
+fi        
