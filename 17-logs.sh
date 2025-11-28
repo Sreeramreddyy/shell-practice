82ffd1267cf9e306fa1643df14b9b552f5b7ddb5
Sreeram Reddy <sreeramlearning25@gmail.com>
2025-11-28 23:58:24 +0530

    Variables

--- 17-logs.sh
+++ 17-logs.sh
@@ -11,6 +11,7 @@ SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOGS_FOLDER
+
 echo "Script started executed at: $(date)"
 
 if [ $USERID -ne 0 ]; then
